f254479ddae7fd980c716c6763e7fee63b76a998
Raymond Pulver IV <root@ecdesign.co>
2014-12-20 09:29:09 -0500

    vm code refactored, core code in vm constructor, and all possible code separated into utility modules

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 
 var toCamelCase = require('../lib/util/to-camel-case'),
+    substituteVars = require('../lib/util/substitute-vars'),
     addNewlines = require('../lib/util/add-newlines');
 describe('jsh utility modules', function () {
   it('should convert to camel-case', function () {
@@ -12,5 +13,11 @@ describe('jsh utility modules', function () {
   });
   it('should add newlines after semicolons and preserve strings', function () {
     expect(addNewlines('console.log(\'{a:1}\');')).to.equal('console.log(\'{a:1}\');\n');
+  });
+  it('should substitute variables', function () {
+    expect(substituteVars('execSync(\'rm $HOME\');')).to.equal('execSync(\'rm \' + HOME + \'\');');
+  });
+  it('should substitute expressions', function () {
+    expect(substituteVars('execSync(\'rm ${HOME + \\\'woop\\\'}\');')).to.equal('execSync(\'rm \' + HOME + \'woop\' + \'\');');
   });
 });
